484dc81336e02653802aab1a34b52c026cd0b294
Andrew Teesdale, Jr <andrew+git@f40c40.com>
2025-10-03 15:25:25 -0700

    added basic map functionality w/ maplibre gl js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Map } from '@vis.gl/react-maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css'
 
 function App() {
@@ -8,26 +8,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Map of Homes Foreclosed By Tax</h1>
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5
+        }}
+        style={{width: 800, height: 600}}
+        mapStyle="https://demotiles.maplibre.org/style.json"
+      />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
